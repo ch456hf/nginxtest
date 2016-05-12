e47fbd775f9b4d75080353cdc91e6edcd8b36608
ch <ch456hf>
2016-05-12 22:21:08 +0800

    modify ngx_http_mytest_upstream_module.c and add some function

--- my_upstream_module/ngx_http_mytest_upstream_module.c
+++ my_upstream_module/ngx_http_mytest_upstream_module.c
@@ -67,6 +67,7 @@ static char *ngx_http_mytest_merge_loc_conf(ngx_conf_t *cf, void *parent, void *
     return NGX_CONF_OK;
 }
 
+//创建发送给上游服务器的http请求，函数指针赋给ngx_http_mytest_conf中的upstream中的create_request指针
 static ngx_int_t mytest_upstream_create_request(ngx_http_request_t *r)
 {
     static ngx_str_t backendQueryLine = ngx_string("Get /search?q=%V HTTP/1.1\r\nHost: www.baidu.com\r\nConnection: close\r\n\r\n");
@@ -92,16 +93,19 @@ static ngx_int_t mytest_upstream_create_request(ngx_http_request_t *r)
     return NGX_OK;
 }
 
+//解析http相应行
 static ngx_int_t mytest_process_status_line(ngx_http_request_t *r)
 {
     size_t len;
     ngx_int_t rc;
     ngx_http_upstream_t *u;
 
+    //保存上文的解析状态
     ngx_http_mytest_ctx_t *ctx = ngx_http_get_module_ctx(r, ngx_http_mytest_upstream_module);
     if(ctx == NULL)
         return NGX_ERROR;
     u = r->upstream;
+    //u->buffer存储的是接收自上游服务器发来的响应内容
     rc = ngx_http_parse_status_line(r, &u->buffer, &ctx->status);
 
     if(rc == NGX_AGAIN)
@@ -135,6 +139,95 @@ static ngx_int_t mytest_process_status_line(ngx_http_request_t *r)
 
 }
 
+static ngx_int_t mytest_upstream_process_header(ngx_http_request_t *r)
+{
+    ngx_int_t rc;
+    ngx_table_elt_t *h;
+    ngx_http_upstream_header_t *hh;
+    ngx_http_upstream_main_conf_t *umcf;
+
+    umcf = ngx_http_get_module_main_conf(r, ngx_http_upstream_module);
+
+    for(;;) {
+        rc = ngx_http_parse_header_line(r, &r->upstream->buffer, 1);
+
+        if(rc == NGX_OK) {
+            h = ngx_list_push(&r->upstream->headers_in.headers);
+            if(h == NULL)
+                return NGX_ERROR;
+
+            h->hash = r->header_hash;
+            
+            h->key.len = r->header_name_end - r->header_name_start;
+            h->value.len = r->header_end - r->header_start;
+
+            h->key.data = ngx_pnalloc(r->pool, h->key.len + 1 + h->value.len + 1 + h->key.len);
+            if(h->key.data == NULL)
+                return NGX_ERROR;
+
+            h->value.data = h->key.data + h->key.len + 1;
+            h->lowcase_key = h->key.data + h->key.len + 1 + h->value.len + 1;
+
+            ngx_memcpy(h->key.data, r->header_name_start, h->key.len);
+            h->key.data[h->key.len] = '\0';
+            ngx_memcpy(h->value.data, r->header_start, h->value.len);
+            h->value.data[h->value.len] = '\0';
+
+            if(h->key.len == r->lowcase_index) {
+                ngx_memcpy(h->lowcase_key, r->lowcase_header, h->key.len);
+            } else {
+                ngx_strlow(h->lowcase_key, h->key.data, h->key.len);
+            }
+
+            hh = ngx_hash_find(&umcf->headers_in_hash, h->hash, h->lowcase_key, h->key.len);
+
+            if(hh && hh->handler(r, h, hh->offset) != NGX_OK) {
+                return NGX_ERROR;
+            }
+            continue;
+        }
+        //下面如果返回NGX_HTTP_PARSE_HEADER_DONE表示响应中的所有http头部都解析完毕，接下来将接收到的都将是http包体
+        if(rc == NGX_HTTP_PARSE_HEADER_DONE) {
+            if(r->upstream->headers_in.server == NULL) {
+                h = ngx_list_push(&r->upstream->headers_in.headers);
+                if(h == NULL)
+                    return NGX_ERROR;
+
+                h->hash = ngx_hash(ngx_hash(ngx_hash(ngx_hash(ngx_hash('s', 'e'), 'r'), 'v'), 'e'), 'r');
+                ngx_str_set(&h->key, "Server");
+                ngx_str_null(&h->value);
+                h->lowcase_key = (u_char *)"server";
+            }
+
+            if(r->upstream->headers_in.date == NULL) {
+                h = ngx_list_push(&r->upstream->headers_in.headers);
+                if(h == NULL)
+                    return NGX_ERROR;
+
+                h->hash = ngx_hash(ngx_hash(ngx_hash('d', 'a'), 't'), 'e');
+
+                ngx_str_set(&h->key, "Date");
+                ngx_str_null(&h->value);
+                h->lowcase_key = (u_char *)"date";
+            }
+            return NGX_OK;
+        }
+
+        //如果返回的NGX_AGAIN，则表示状态机还没有解析到完整的http头部，此时要求upstream模块继续接收新的字符流，然后交给process_header解析
+        if(rc == NGX_AGAIN)
+            return rc;
+
+        ngx_log_error(NGX_LOG_ERR, r->connection->log, 0, "upstream send invalid header");
+        return NGX_HTTP_UPSTREAM_INVALID_HEADER;
+    }
+}
+
+//finalize_request是再当请求结束的时候回调用的，此时用来释放一定资源，比如打开的句柄。
+static void mytest_finalize_request(ngx_http_request_t *r, ngx_int_t rc)
+{
+    ngx_log_error(NGX_LOG_DEBUG, r->connection->log, 0, "mytest_upstream_finalize_request");
+}
+
 static ngx_http_module_t ngx_http_mytest_module_ctx = {
     NULL,
     NULL,
@@ -142,8 +235,8 @@ static ngx_http_module_t ngx_http_mytest_module_ctx = {
     NULL,
     NULL,
     NULL,
-    NULL,
-    NULL
+    ngx_http_mytest_create_loc_conf,
+    ngx_http_mytest_merge_loc_conf
 };
 
 ngx_module_t ngx_http_mytest_upstream_module = {
@@ -174,7 +267,11 @@ ngx_http_mytest(ngx_conf_t *cf, ngx_command_t *cmd, void *conf)
     return NGX_CONF_OK;
 }
 
+static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r)
+{
+}
 
+/*用做mytest模块的回调函数，此处注释掉，上面的是重新写的upstream模块的回调函数
 static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r)
 {
     if (!(r->method & (NGX_HTTP_GET | NGX_HTTP_HEAD))) {
@@ -185,7 +282,7 @@ static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r)
     if (rc != NGX_OK) {
         return rc;
     }
-    /*单纯的发送一串字符串用此处
+    //单纯的发送一串字符串用此处
     ngx_str_t type = ngx_string("text/plain");
     ngx_str_t response = ngx_string("Hello World");
     r->headers_out.status = NGX_HTTP_OK;
@@ -209,9 +306,8 @@ static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r)
     ngx_chain_t out;
     out.buf = b;
     out.next = NULL;
-    */
-    /*用http发送文件用此处*/
-
+    
+    //用http发送文件用此处
     ngx_buf_t *b;
     b = ngx_palloc(r->pool, sizeof(ngx_buf_t));
 
@@ -264,4 +360,4 @@ static ngx_int_t ngx_http_mytest_handler(ngx_http_request_t *r)
 
     return ngx_http_output_filter(r, &out);
 }
-
+*/
